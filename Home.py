5a6913fd62696285c3476b72fc95df1bc7ff6bdd
Adrian Magana <nan.mruiz.05@gmail.com>
2025-05-28 13:42:07 -0600

    Arreglado logo y agregado página contacto

---
+++ Home.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.set_page_config(page_title="App de Diagramas", layout="wide")
+
+st.title("📐Diagramas de Interacción")
+st.markdown("""
+Bienvenido a la **App de Columnas de Concreto Reforzado**  
+Usa el menú lateral para seleccionar el tipo de sección.  
+Esta app calcula y genera diagramas de interacción para columnas:
+- Rectangulares
+- Circulares
+- Personalizadas
+
+Desarrollado por: *Adrián Magaña Ruiz,Hugo Hernández Barrios*
+""")
+
+
+
+import streamlit as st
+
+# Después de todo el contenido, pones:
+
+st.markdown("---")  # línea divisoria opcional
+st.image(r"C:\Users\nan_m\Downloads\App_Diagramas_Streamlit\mi_app_diagramas\FIC.png", width=150)
+st.markdown("Facultad de Ingeniería Civil UMSNH")
+
+
+
---
+++ pages/5_Contacto.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Contacto")
+st.write("Información sobre los autores.")
+st.write("Contacto Adrián Magaña Ruiz: 2100784X@umich.mx,nan.mruiz.05@gmail.com,4439552218")
+st.write("Contacto Dr. Hugo Hernández Barrios: hugo.hernandez@umich.mx")
